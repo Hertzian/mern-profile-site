86261d45c9fee9aecb5cfe0941c9a4f1a68748f7
Hertzian <tallitosan@gmail.com>
2021-10-01 21:58:01 -0500

    removed place context

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,17 @@ import HomePage from './pages/templates/HomePage'
 import ProjectPage from './pages/templates/ProjectPage'
 import Login from './pages/templates/Login'
 import AdminPage from './pages/templates/AdminPage'
-import PlaceState from './context/PlaceState'
 
 function App() {
   return (
-    <PlaceState>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/project/:projectId' component={ProjectPage} />
-          <Route exact path='/mamalon' component={Login} />
-          <Route path='/admin' component={AdminPage} />
-        </Switch>
-      </Router>
-    </PlaceState>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/project/:projectId' component={ProjectPage} />
+        <Route exact path='/mamalon' component={Login} />
+        <Route path='/admin' component={AdminPage} />
+      </Switch>
+    </Router>
   )
 }
 
