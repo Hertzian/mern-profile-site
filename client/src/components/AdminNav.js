afd6f7d00fa1ee573a6129260e8463096149b8ad
Hertzian <tallitosan@gmail.com>
2021-10-13 22:03:06 -0500

    fixed adminNav props inheritance, privateRoute component, fixed setAuthToken to make requests

--- client/src/components/AdminNav.js
+++ client/src/components/AdminNav.js
@@ -1,45 +1,64 @@
-import { useState } from 'react'
+import { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { logout } from '../context/userActions'
+import { UserContext } from '../context/UserContext'
 
-const AdminNav = ({ menuState }) => {
-  const [userButton, setUserButton] = useState(false)
-  const handleLogout = () => setUserButton(!userButton)
+class AdminNav extends Component {
+  static contextType = UserContext
+  constructor(props) {
+    super(props)
+    this.state = { isOpen: false }
+    this.toggleOpen = this.toggleOpen.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
+  }
 
-  return (
-    <nav className='sb-topnav navbar navbar-expand navbar-dark bg-dark'>
-      <a className='navbar-brand' href='index.html'>
-        Admin Panel
-      </a>
-      <button
-        className='btn btn-link btn-sm order-1 order-lg-0'
-        id='sidebarToggle'
-        onClick={menuState}
-      >
-        <i className='fas fa-bars'></i>
-      </button>
-      <div className='d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0'></div>
-      <ul className='navbar-nav ml-auto ml-md-0'>
-        <li className='nav-item dropdown'>
-          <span
-            className='nav-link dropdown-toggle'
-            id='userDropdown'
-            onClick={handleLogout}
-          >
-            <i className='fas fa-user fa-fw'></i>
-          </span>
-          <div
-            className={`dropdown-menu dropdown-menu-right ${
-              userButton && 'show'
-            }`}
-          >
-            <Link className='dropdown-item' to={'/'}>
-              Logout
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </nav>
-  )
+  toggleOpen() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  handleLogout() {
+    this.context.logout()
+    logout()
+    this.props.history.replace('/mamalon/login')
+  }
+
+  render() {
+    return (
+      <nav className='sb-topnav navbar navbar-expand navbar-dark bg-dark'>
+        <Link to='/' className='navbar-brand'>
+          Admin Panel
+        </Link>
+        <button
+          className='btn btn-link btn-sm order-1 order-lg-0'
+          id='sidebarToggle'
+          onClick={this.props.menuState}
+        >
+          <i className='fas fa-bars'></i>
+        </button>
+        <div className='d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0'></div>
+        <ul className='navbar-nav ml-auto ml-md-0'>
+          <li className='nav-item dropdown'>
+            <span
+              className='nav-link dropdown-toggle'
+              id='userDropdown'
+              onClick={this.toggleOpen}
+            >
+              <i className='fas fa-user fa-fw'></i>
+            </span>
+            <div
+              className={`dropdown-menu dropdown-menu-right ${
+                this.state.isOpen && 'show'
+              }`}
+            >
+              <button onClick={this.handleLogout} className='dropdown-item'>
+                Logout
+              </button>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 }
 
 export default AdminNav
