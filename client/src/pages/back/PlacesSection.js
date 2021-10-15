1f8191ce822f7cd4643895ddb18c6cdaf96fd87a
Hertzian <tallitosan@gmail.com>
2021-10-15 01:56:59 -0500

    begin with markup in PlacesSection & fixed props in AdminPage

--- client/src/pages/back/PlacesSection.js
+++ client/src/pages/back/PlacesSection.js
@@ -1,21 +1,55 @@
+import { Component } from 'react'
 import AdminPage from '../templates/AdminPage'
 import Card from '../../components/Card'
 import ButtonOpenModal from '../../components/ButtonOpenModal'
+import PlaceModal from '../../components/PlaceModal'
 
-const PlacesSection = (props) => {
-  return (
-    <AdminPage {...props}>
-      <Card header={'Places'}>
-        <ButtonOpenModal
-          toggle='modal'
-          target='#new-place'
-          className='btn btn-primary mb-2'
-          legend='New place'
-          dataid='new-place'
-        />
-      </Card>
-    </AdminPage>
-  )
+class PlacesSection extends Component {
+  render() {
+    return (
+      <AdminPage {...this.props}>
+        <Card header={'Places'}>
+          <ButtonOpenModal
+            target='new-place'
+            color='primary mb-2'
+            label='New place'
+          />
+          <PlaceModal target='new-place' isModify={true} />
+          <table class='table table-bordered'>
+            <thead>
+              <tr>
+                <th scope='col'>Company</th>
+                <th scope='col'>Job</th>
+                <th scope='col'>Year</th>
+                <th scope='col'>Show</th>
+                <th scope='col'>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Mark</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>
+                  <ButtonOpenModal
+                    target={`edit-place-`}
+                    color='primary'
+                    label='Update'
+                  />
+                  <ButtonOpenModal
+                    target={`delete-place-`}
+                    color='danger ml-2'
+                    label='delete'
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Card>
+      </AdminPage>
+    )
+  }
 }
 
 export default PlacesSection
--- client/src/pages/templates/AdminPage.js
+++ client/src/pages/templates/AdminPage.js
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import AdminMenu from '../../components/AdminMenu'
 import AdminNav from '../../components/AdminNav'
+import setAuthToken from '../../utils/setAuthToken'
 import '../../styles/tables.css'
 import '../../styles/back.css'
 
 const AdminPage = (props) => {
   const [openMenu, setOpenMenu] = useState(true)
-
   const handleMenu = () => setOpenMenu(!openMenu)
+  const token = localStorage.token
+
+  if (token) setAuthToken(token)
 
   return (
     <div className={`sb-nav-fixed ${!openMenu && 'sb-sidenav-toggled'}`}>
