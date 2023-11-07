7ba605fb82769dc271a787056d76ff8f81ba3a5b
Eduardo Aguilar <tallitosan@gmail.com>
2023-11-06 22:41:37 -0600

    add skill api & slice and getToken aux function

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,27 +5,35 @@ import { frontPageApi } from './apis/frontPageApi'
 import { authApi } from './apis/authApi'
 import { usersApi } from './apis/usersApi'
 import { placesApi } from './apis/placesApi'
+import { skillsApi } from './apis/skillsApi'
 
 import { authReducer, setLoginCredentials, logout } from './slices/authSlice'
 import { usersReducer, updateStateProfile, setStateProfile } from './slices/usersSlice'
 import { placesReducer, setStateAllPlaces, setStateNewPlace, updateStatePlace, deleteStatePlace } from './slices/placesSlice'
+import { setStateSkills, setStateNewSkill, updateStateSkill, deleteStateSkill, skillsReducer } from './slices/skillsSlice'
 
 export const store = configureStore({
   reducer: {
     authSlice: authReducer,
     usersSlice: usersReducer,
     placesSlice: placesReducer,
+    skillsSlice: skillsReducer,
+
     [frontPageApi.reducerPath]: frontPageApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
-    [placesApi.reducerPath]: placesApi.reducer
+    [placesApi.reducerPath]: placesApi.reducer,
+    [skillsApi.reducerPath]: skillsApi.reducer
+
   },
+
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware()
       .concat(frontPageApi.middleware)
       .concat(authApi.middleware)
       .concat(usersApi.middleware)
       .concat(placesApi.middleware)
+      .concat(skillsApi.middleware)
   }
 })
 
@@ -35,8 +43,10 @@ export {
   setLoginCredentials, logout,
   updateStateProfile, setStateProfile,
   setStateAllPlaces, setStateNewPlace, updateStatePlace, deleteStatePlace,
+  setStateSkills, setStateNewSkill, updateStateSkill, deleteStateSkill,
 }
 export { useGetGeneralProfileQuery, } from './apis/frontPageApi'
 export { useLoginMutation } from './apis/authApi'
 export { useGetProfileQuery, useUpdateProfileMutation } from './apis/usersApi'
-export { useGetAllPlacesQuery, useGetSinglePlaceQuery, useCreatePlaceMutation, useUpdatePlaceMutation, useDeletePlaceMutation } from './apis/placesApi'
+export { useGetPlacesQuery, useCreatePlaceMutation, useUpdatePlaceMutation, useDeletePlaceMutation } from './apis/placesApi'
+export { useGetSkillsQuery, useCreateSkillMutation, useUpdateSkillMutation, useDeleteSkillMutation } from './apis/skillsApi'
